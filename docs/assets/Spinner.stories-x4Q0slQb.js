3d18e2dd143a926bee2085b0d27d05735c5e24d8
vyrale17 <preteuses-genou-94@icloud.com>
2026-02-05 13:08:03 +0000

    Deploying to docs from @ vyrale17/pass-culture-main-all@fb2d075c76ed225780f762e232be3b32ab784fd9 🚀

---
+++ docs/assets/Spinner.stories-x4Q0slQb.js
@@ -0,0 +1,5 @@
+import{V as e}from"./index-B1TV06f5.js";import{S as o}from"./Spinner-BvOsGwgH.js";import"./iframe-CTYXmkIP.js";import"./preload-helper-PPVm8Dsz.js";import"./chunk-EPOLDU6W-B6iDpUT7.js";import"./jsx-runtime-fBYdV91l.js";import"./index-CMLNM0uR.js";import"./stroke-pass-CALgybTM.js";import"./SvgIcon-jeh_DQK7.js";const d={title:"@/ui-kit/Spinner",decorators:[e],component:o},r={args:{message:"Chargement en cours"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    message: 'Chargement en cours'
+  }
+}`,...r.parameters?.docs?.source}}};const g=["Default"];export{r as Default,g as __namedExportsOrder,d as default};
---
+++ docs/assets/Tooltip.stories-jnNQzgqd.js
@@ -0,0 +1,6 @@
+import{j as o}from"./jsx-runtime-fBYdV91l.js";import{T as e}from"./Tooltip-YtCRjN_n.js";import{B as i}from"./Button-DjNutbWK.js";import"./iframe-CTYXmkIP.js";import"./preload-helper-PPVm8Dsz.js";import"./index-CMLNM0uR.js";import"./SvgIcon-jeh_DQK7.js";import"./stroke-pass-CALgybTM.js";import"./chunk-EPOLDU6W-B6iDpUT7.js";const f={title:"@/ui-kit/Tooltip",component:e,decorators:[r=>o.jsx("div",{style:{padding:"4rem"},children:o.jsx(r,{})})]},t={args:{content:"Contenu du tooltip",children:o.jsx(i,{label:"Je peux afficher un tooltip."})}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    content: 'Contenu du tooltip',
+    children: <Button label="Je peux afficher un tooltip." />
+  }
+}`,...t.parameters?.docs?.source}}};const x=["Default"];export{t as Default,x as __namedExportsOrder,f as default};
